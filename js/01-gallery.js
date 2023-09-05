a952a34e9e702de291ea394e06c069e93183791f
NadiiaChernokur <130999859+NadiiaChernokur@users.noreply.github.com>
2023-09-05 23:50:25 +0200

    Add files via upload

---
+++ js/01-gallery.js
@@ -0,0 +1,60 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const createGallery = createListGallery(galleryItems);
+const container = document.querySelector("ul.gallery");
+
+container.insertAdjacentHTML("afterbegin", createGallery);
+container.addEventListener("click", openModalClick);
+
+function  openModalClick(event) {
+  if(event.target.nodeName !== "IMG"){
+    return;
+   }
+   event.preventDefault();
+
+   const instance = basicLightbox.create(`
+      <div class="modal">
+       <img src="${event.target.dataset.source}" alt="${event.target.alt}"/>
+       </div>
+     `, {
+      onShow: (instance) => {
+      document.addEventListener('keydown', closeModal)
+      
+     },
+      onClose: (instance) => {
+        document.removeEventListener('keydown', closeModal)
+        // document.removeEventListener('click', closeModalImg)
+      }
+     })
+     instance.show()
+     
+     function closeModal(e) {
+      if (e.code === 'Escape') {
+        instance.close()
+       }
+      }
+
+      // function closeModalImg(e) {
+      //   if (e.target.nodeName === 'IMG') {
+      //     instance.close()
+      //    }
+      //   }
+};
+   
+function createListGallery(images)  { 
+    return images.map(({preview, description, original}) => {
+        return `
+        <li class="gallery__item ">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    }).join('');
+    }; 
+    console.log(createGallery)
---
+++ js/02-lightbox.js
@@ -0,0 +1,25 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+const markup = createMarkup(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+ galleryList.insertAdjacentHTML("beforeend", markup)
+ 
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"",captionDelay: 100, captionPosition: "top", navText: ['-','+']});
+
+
+function createMarkup(arr) {
+return arr.map(({ preview, original, description}) => {
+      return `
+      <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+    </li>
+      ` 
+    }).join("");
+}
+
+
